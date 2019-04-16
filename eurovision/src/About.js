cdf10c591d60c85dc929e73ded3536ca63654b08
mikko kotamies <mikko.kotamies@avarko.com>
2019-04-16 19:36:04 +0300

    Update about text

--- eurovision/src/About.js
+++ eurovision/src/About.js
@@ -5,15 +5,12 @@ const AboutComponent = () => (
   <Container text className='Base-style'>
     <Header as='h2' className='Base-style'>About Viisut.fi</Header>
     <p>
-        Viisut provides analysis of Eurovision 2017. Analysis is based on Youtube views. In our model highest view count means the highest
-        probability to win. 
+        Viisut provides analysis of Eurovision 2019. Analysis is based on Youtube views.
     </p>
     <h2>Why?</h2>
     <p>
-        One of the authors got this idea on tuesday night.
-        The next day he spoke with co-worker and they decided to implement this page just for fun. 
-        Please note that Viisut was created quickly during the last week of Eurovision 2017. Hopefully next year we can provide more
-        comprehensive analysis based on the data we collect this year.
+        One of the authors got this idea on tuesday night 2017.
+        The next day he spoke with co-worker and they decided to implement this page just for fun.
     </p>
   </Container>
 )
