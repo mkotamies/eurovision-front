0f55d405688f1279cab0f92321367e94d72daff6
mikko kotamies <mikko.kotamies@avarko.com>
2019-04-16 18:17:15 +0300

    Add loading spinner

--- eurovision/src/App.js
+++ eurovision/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import CardComponent from './Card.js';
 import Axios from 'axios';
 import { Card } from 'semantic-ui-react';
-import AboutComponent from './About.js'
-import { Menu } from 'semantic-ui-react'
+import AboutComponent from './About.js';
+import { Menu } from 'semantic-ui-react';
+import { PacmanLoader } from 'react-spinners';
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +13,14 @@ class App extends Component {
     this.state = {
       data: [],
       activeItem: 'results',
+      fetching: true,
     }
   }
 
   componentDidMount(){
     Axios.get('https://3ahttr5986.execute-api.eu-central-1.amazonaws.com/prod/status')
     .then(response => response.data.body)
-    .then(data => this.setState({data: data}))
+    .then(data => this.setState({data: data, fetching: false}))
     .catch(error => console.log(error));
   }
 
@@ -42,14 +44,21 @@ class App extends Component {
     }
     return (
       <div className="App">
-          <Menu className='Menu'>
-            <Menu.Item header className='Base-style'>Viisut </Menu.Item >
-            <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} className='Base-style' />
-            <Menu.Item name='results' active={activeItem === 'results'} onClick={this.handleItemClick} className='Base-style' />
-          </Menu>
-        <p className="App-intro">
-          <Card.Group className={styl}>{results}</Card.Group>
-        </p>
+        <Menu className='Menu'>
+          <Menu.Item header className='Base-style'>Viisut </Menu.Item >
+          <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} className='Base-style' />
+          <Menu.Item name='results' active={activeItem === 'results'} onClick={this.handleItemClick} className='Base-style' />
+        </Menu>
+        <div className="App-intro">
+          {this.state.fetching &&
+            <div style={{ width: '80px', margin: '0 auto', height: '80px' }}>
+              <PacmanLoader loading={this.state.fetching} color={'#747397'} />
+            </div>
+          } 
+          <div>
+            <Card.Group className={styl}>{results}</Card.Group>
+          </div>
+        </div>
       </div>
     );
   }
