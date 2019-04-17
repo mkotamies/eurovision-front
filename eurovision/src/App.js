5a68514d07631d172f92e4f0d5f28ec258d7b10a
mikko kotamies <mikko.kotamies@avarko.com>
2019-04-17 15:12:29 +0300

    Create chart for top 5 contestants

--- eurovision/src/App.js
+++ eurovision/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import { List, Menu } from 'semantic-ui-react';
 import { PacmanLoader } from 'react-spinners';
-import ListItem from './ListItem';
+import { List, Menu } from 'semantic-ui-react';
+
 import AboutComponent from './About';
+import Chart from './Chart';
+import ListItem from './ListItem';
 import './App.css';
 
 class App extends Component {
@@ -11,32 +13,41 @@ class App extends Component {
     super(props)
     this.state = {
       data: [],
-      activeItem: 'results',
+      activeItem: 'topFive',
       fetching: true,
+      screenWidth: null,
     }
   }
 
   componentDidMount(){
     Axios.get('https://3ahttr5986.execute-api.eu-central-1.amazonaws.com/prod/status')
     .then(response => response.data.body)
-    .then(data => this.setState({data: data, fetching: false}))
+    .then(data => this.setState({data: data, fetching: false, screenWidth: window.innerWidth}))
     .catch(error => console.log(error));
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
   }
 
   changeMenu(param){
     this.setState({menu: param});
   }
 
+  resize() {
+    this.setState({ screenWidth: window.innerWidth }); // this might be useless for normal users
+  }
+
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
+    const hasResults = this.state.data && this.state.data.length > 0;
     return (
       <div className="App">
         <Menu className='Menu'>
           <Menu.Item header className='Base-style'>Viisut </Menu.Item >
           <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} className='Base-style' />
           <Menu.Item name='results' active={activeItem === 'results'} onClick={this.handleItemClick} className='Base-style' />
+          <Menu.Item name='topFive' active={activeItem === 'topFive'} onClick={this.handleItemClick} className='Base-style' />
         </Menu>
         <div className="App-intro">
           {this.state.fetching &&
@@ -46,14 +57,19 @@ class App extends Component {
               </div>
             </div>
           }
-          {activeItem === 'results' && this.state.data && this.state.data.length > 0 ?
+          {activeItem === 'results' && hasResults &&
             <List>
                 {this.state.data.map((elem, index) => (
                   <ListItem key={index} data={elem} index={index} />
                 ))}
-            </List>:
-            <div className='Card-main'><AboutComponent /></div>
+            </List>
           }
+          {activeItem === 'about' && <div className='Card-main'><AboutComponent /></div>}
+          {activeItem === 'topFive' && hasResults &&
+            <div className='Card-main'>
+              <div style={{ color: 'white' }}>Chart provides information about top five contestants and their view counts.</div>
+              <Chart data={this.state.data} screenWidth={this.state.screenWidth} />
+            </div>}
         </div>
       </div>
     );
